3f3b598da7c49fdad3febee8f4820f32d2292997
developerSid <developerSid@gmail.com>
2018-07-27 13:02:12 -0500

    Put in the skeleton for the App

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -4,6 +4,15 @@ import 'package:flutter/material.dart';
 
 // create a class that will be our custom widget
 // this class must extend the `StatelessWidget` base class
+class App extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  }
+
+}
+
 
 // must define a build method that returns
 // the widgets that this widget will show
