81b7e782a63426239e99509a6f15f13fa6c1ebc6
developerSid <developerSid@gmail.com>
2018-07-27 12:36:07 -0500

    Began app
    Setup Scaffold and AppBar

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -1,15 +1,11 @@
-// I need to import a helper library
-// from flutter to get content on the screen
 import 'package:flutter/material.dart';
 
-// Define a main function to run when our app starts
 void main() {
   final app = MaterialApp(
-    home: Text('Hi there!')
+    home: new Scaffold(
+      appBar: AppBar(),
+    )
   );
 
   runApp(app);
 }
-// Create a new text widget to show some text on the screen
-
-// tak that widget and get it on the screen
