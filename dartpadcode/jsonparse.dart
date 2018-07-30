16e08f96cfe256b212124f8218eb2e35c197c4a9
developerSid <developerSid@gmail.com>
2018-07-30 13:21:15 -0500

    Added image model for capturing the data from the parsed JSON

--- dartpadcode/jsonparse.dart
+++ dartpadcode/jsonparse.dart
@@ -1,8 +1,24 @@
+
 import 'dart:convert';
 
 void main() {
   final rawJson = '{"url": "http://blah.jpg", "id": 1}';
   final parsedJson = json.decode(rawJson);
+  final singleImage = ImageModel(parsedJson['id'], parsedJson['url']);
   
-  print(parsedJson['url']);
+  print(singleImage);
+  print(singleImage.id);
+  print(singleImage.url);
 }
+
+class ImageModel {
+  int id;
+  String url;
+  
+  ImageModel(this.id, this.url);
+  
+  String toString() {
+    return "$id -> $url";
+  }
+}
+
