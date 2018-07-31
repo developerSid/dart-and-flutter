0027d5081511f28317147f8079727d35883463d9
developerSid <developerSid@gmail.com>
2018-07-31 12:47:58 -0500

    Began creating an ImageList widget

---
+++ pics/lib/src/widgets/image_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:pics/src/models/image_models.dart';
+
+class ImageList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+  }
+}
