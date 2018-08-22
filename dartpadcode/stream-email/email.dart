8ccc02515027808e1ab0c267667a09e4cb173920
developerSid <developerSid@gmail.com>
2018-08-22 12:59:40 -0500

    Added in validation via a transformer

--- dartpadcode/stream-email/email.dart
+++ dartpadcode/stream-email/email.dart
@@ -1,11 +1,23 @@
+import 'dart:async';
 import 'dart:html';
 
 void main(){
   final InputElement input = querySelector('input');
   final DivElement div = querySelector('div');
   
+  final validator = StreamTransformer.fromHandlers(
+    handleData: (inputValue, sink) {
+      if (inputValue.contains('@')) {
+        sink.add(inputValue);
+      } else {
+        sink.addError('Enter a valid email');
+      }
+    }
+  );
+  
   input.onInput
-    .listen((dynamic event) => print(event.target.value))
+    .map((dynamic event) => event.target.value)
+    .transform(validator)
   ;
 }
 
