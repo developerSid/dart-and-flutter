dce40d3e820163ac89b37e5630e388065a755117
developerSid <developerSid@gmail.com>
2018-11-03 16:54:31 -0500

    Added rxdart package for combining streams
    Added a new stream to the bloc that combines the email and password

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:rxdart/rxdart.dart';
 
 import 'package:login_bloc/src/blocs/validators.dart';
 
@@ -9,6 +10,7 @@ class Bloc extends Object with Validators {
    // Add data to stream
    Stream<String> get email => _email.stream.transform(validateEmail);
    Stream<String> get password => _password.stream.transform(validatePassword);
+   Stream<bool> get submitValid => Observable.combineLatest2(email, password, (es, ps) => true );
 
    // Change data
    Function(String) get changeEmail => _email.sink.add;
